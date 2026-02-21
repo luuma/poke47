80cd89e700cc905e6028be1d5337d9baa73a2c86
luuma <31407427+luuma@users.noreply.github.com>
2026-02-21 00:03:29 +0000

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -585,7 +585,8 @@ static void CB2_InitBattleInternal(void)
         TryFormChange(i, B_SIDE_OPPONENT, FORM_CHANGE_BEGIN_BATTLE);
     }
     if (!(gBattleTypeFlags & BATTLE_TYPE_TRAINER))//  (BATTLE_TYPE_TRAINER | BATTLE_TYPE_LINK | BATTLE_TYPE_RECORDED)??
-	TryFormChange(0, B_SIDE_OPPONENT, FORM_CHANGE_BEGIN_WILD_ENCOUNTER);// This only needs to be done for an opponent as the player's minior is reset to core on end of battle. Minior is always given in core form.
+	TryFormChange(0, B_SIDE_OPPONENT, FORM_CHANGE_BEGIN_WILD_ENCOUNTER); // This only needs to be done for an opponent as the player's minior is reset to core on end of battle. Minior is always given in core form.
+	TryFormChange(1, B_SIDE_OPPONENT, FORM_CHANGE_BEGIN_WILD_ENCOUNTER); // doubles
     if (TESTING)
     {
         gPlayerPartyCount = CalculatePartyCount(gPlayerParty);
