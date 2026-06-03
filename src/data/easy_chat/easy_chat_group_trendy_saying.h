eb0f536097b6ec63ced22188e210024c62778415
luuma <31407427+luuma@users.noreply.github.com>
2026-06-03 20:33:05 +0100

    Added outdated internet culture

--- src/data/easy_chat/easy_chat_group_trendy_saying.h
+++ src/data/easy_chat/easy_chat_group_trendy_saying.h
@@ -7,13 +7,13 @@ const struct EasyChatWordInfo gEasyChatGroup_TrendySaying[] = {
     },
     [EC_INDEX(EC_WORD_YES_SIR_EXCL)] =
     {
-        .text = COMPOUND_STRING("YES, SIR!"),
+        .text = COMPOUND_STRING("YEET!"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_AVANT_GARDE),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_AVANT_GARDE)] =
     {
-        .text = COMPOUND_STRING("AVANT GARDE"),
+        .text = COMPOUND_STRING("AMONG US"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_BABY),
         .enabled = TRUE,
     },
@@ -25,67 +25,67 @@ const struct EasyChatWordInfo gEasyChatGroup_TrendySaying[] = {
     },
     [EC_INDEX(EC_WORD_MUCH_OBLIGED)] =
     {
-        .text = COMPOUND_STRING("MUCH OBLIGED"),
+        .text = COMPOUND_STRING("MY IMMORTAL"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_CANCEL),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_YEEHAW_EXCL)] =
     {
-        .text = COMPOUND_STRING("YEEHAW!"),
+        .text = COMPOUND_STRING("YIFF"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_COUPLE),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_MEGA)] =
     {
-        .text = COMPOUND_STRING("MEGA"),
+        .text = COMPOUND_STRING("MOGGED"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_DESTINY),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_1_HIT_KO_EXCL)] =
     {
-        .text = COMPOUND_STRING("1-HIT KO!"),
+        .text = COMPOUND_STRING("1337"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_FEVER),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_DESTINY)] =
     {
-        .text = COMPOUND_STRING("DESTINY"),
+        .text = COMPOUND_STRING("DEEZNUTS"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_FLATTEN),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_CANCEL)] =
     {
         .text = COMPOUND_STRING("CANCEL"),
-        .alphabeticalOrder = EC_INDEX(EC_WORD_HAPPENING),
+        .alphabeticalOrder = EC_INDEX(EC_WORD_HASSLE),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_NEW)] =
     {
-        .text = COMPOUND_STRING("NEW"),
-        .alphabeticalOrder = EC_INDEX(EC_WORD_HASSLE),
+        .text = COMPOUND_STRING("OMEGALUL"),
+        .alphabeticalOrder = EC_INDEX(EC_WORD_HEART),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_FLATTEN)] =
     {
-        .text = COMPOUND_STRING("FLATTEN"),
-        .alphabeticalOrder = EC_INDEX(EC_WORD_HEART),
+        .text = COMPOUND_STRING("FUCK"),
+        .alphabeticalOrder = EC_INDEX(EC_WORD_HAPPENING),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_KIDDING)] =
     {
-        .text = COMPOUND_STRING("KIDDING"),
+        .text = COMPOUND_STRING("KAPPA"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_HIP_AND),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_LOSER)] =
     {
-        .text = COMPOUND_STRING("LOSER"),
+        .text = COMPOUND_STRING("LOSS"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_KIDDING),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_LOSING)] =
     {
-        .text = COMPOUND_STRING("LOSING"),
+        .text = COMPOUND_STRING("LULZ"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_KNOCKOUT),
         .enabled = TRUE,
     },
@@ -103,25 +103,25 @@ const struct EasyChatWordInfo gEasyChatGroup_TrendySaying[] = {
     },
     [EC_INDEX(EC_WORD_SHAKE)] =
     {
-        .text = COMPOUND_STRING("SHAKE"),
+        .text = COMPOUND_STRING("SWIGGITY"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_LOSING),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_SHADY)] =
     {
-        .text = COMPOUND_STRING("SHADY"),
+        .text = COMPOUND_STRING("SWAG"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_MEGA),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_UPBEAT)] =
     {
-        .text = COMPOUND_STRING("UPBEAT"),
+        .text = COMPOUND_STRING("UPDOG"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_MODERN),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_MODERN)] =
     {
-        .text = COMPOUND_STRING("MODERN"),
+        .text = COMPOUND_STRING("MEME"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_MUCH_OBLIGED),
         .enabled = TRUE,
     },
@@ -133,67 +133,67 @@ const struct EasyChatWordInfo gEasyChatGroup_TrendySaying[] = {
     },
     [EC_INDEX(EC_WORD_BANG)] =
     {
-        .text = COMPOUND_STRING("BANG"),
+        .text = COMPOUND_STRING("BROHOOF"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_OLD),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_KNOCKOUT)] =
     {
-        .text = COMPOUND_STRING("KNOCKOUT"),
+        .text = COMPOUND_STRING("KUNG POW"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_SHADY),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_HASSLE)] =
     {
-        .text = COMPOUND_STRING("HASSLE"),
+        .text = COMPOUND_STRING("GYATT"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_SHAKE),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_WINNER)] =
     {
-        .text = COMPOUND_STRING("WINNER"),
+        .text = COMPOUND_STRING("WINRAR"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_SMELL_YA),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_FEVER)] =
     {
-        .text = COMPOUND_STRING("FEVER"),
-        .alphabeticalOrder = EC_INDEX(EC_WORD_UGLY),
+        .text = COMPOUND_STRING("DOGE"),
+        .alphabeticalOrder = EC_INDEX(EC_WORD_UPBEAT),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_WANNABE)] =
     {
-        .text = COMPOUND_STRING("WANNABE"),
-        .alphabeticalOrder = EC_INDEX(EC_WORD_UPBEAT),
+        .text = COMPOUND_STRING("WALUIGI"),
+        .alphabeticalOrder = EC_INDEX(EC_WORD_UGLY),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_BABY)] =
     {
-        .text = COMPOUND_STRING("BABY"),
+        .text = COMPOUND_STRING("BORAT"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_WANNABE),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_HEART)] =
     {
-        .text = COMPOUND_STRING("HEART"),
+        .text = COMPOUND_STRING("GLOMP"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_WINNER),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_OLD)] =
     {
-        .text = COMPOUND_STRING("OLD"),
+        .text = COMPOUND_STRING("POGCHAMP"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_YEEHAW_EXCL),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_YOUNG)] =
     {
-        .text = COMPOUND_STRING("YOUNG"),
+        .text = COMPOUND_STRING("YO MAMA"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_YES_SIR_EXCL),
         .enabled = TRUE,
     },
     [EC_INDEX(EC_WORD_UGLY)] =
     {
-        .text = COMPOUND_STRING("UGLY"),
+        .text = COMPOUND_STRING("UR MOM"),
         .alphabeticalOrder = EC_INDEX(EC_WORD_YOUNG),
         .enabled = TRUE,
     },
